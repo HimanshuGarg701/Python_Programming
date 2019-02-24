161b09b1f7b0e05acfe772ac6a795555bad0e9c1
Himanshu Garg <40909157+HimanshuGarg701@users.noreply.github.com>
2019-02-24 14:32:47 -0800

    Practice Question

--- bill.py
+++ bill.py
@@ -2,6 +2,7 @@ x = input("enter the name of the item: ")
 y = float(input("enter the quantity: "))
 z = float(input("enter the price per unit: "))
 money = y*z
+
 print("***********************************BILL***********************************")
 print(str(x)+ " "+"Quantity: "+str(x)+"                            "+"Price: "+str(z))
 print("                                                                              ")
